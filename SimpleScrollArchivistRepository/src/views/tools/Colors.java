d0f88819151043723b21e66756314ba3824b85bd
Mathaus-afpa <mathieu.hoarau@afpa-dev-pompey.fr>
2024-08-13 13:06:11 +0200

    feat(SSAR, Colors.java, CREATE, class): Ecriture de la classe.

---
+++ SimpleScrollArchivistRepository/src/views/tools/Colors.java
@@ -0,0 +1,25 @@
+package com.scrollarchivist.simplescrollarchivistrepository.views.tools;
+import java.awt.Color;
+/**
+ * [Colors] - class
+ * @author Mathaus
+ */
+public class Colors {
+    // <editor-fold defaultstate="collapsed" desc="Constructeurs">
+    
+    // </editor-fold>
+    // <editor-fold defaultstate="collapsed" desc="Proprietes">
+    public final static Color IVORY = new Color(255, 255, 240);
+    public final static Color BLUE = new Color(91, 206, 250);
+    public final static Color PINK = new Color(245, 169, 184);
+    // </editor-fold>
+    // <editor-fold defaultstate="collapsed" desc="Methodes">
+    
+    // </editor-fold>
+    // <editor-fold defaultstate="collapsed" desc="Getter">
+    
+    // </editor-fold>
+    // <editor-fold defaultstate="collapsed" desc="Setter">
+    
+    // </editor-fold>
+}
