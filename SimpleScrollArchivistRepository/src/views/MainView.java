8534b396e58a8e91ceb7e9c0f356245a21513eb6
Mathaus-afpa <mathieu.hoarau@afpa-dev-pompey.fr>
2024-08-13 13:10:45 +0200

    feat(SSAR, MainView.java, CREATE, function): Ecriture de newWindow(). feat(SSAR, MainView.java, CREATE, function): Ecriture de newBody(). usefeat(SSAR, MainView.java, UPDATE, function): Modification de show().

--- SimpleScrollArchivistRepository/src/views/MainView.java
+++ SimpleScrollArchivistRepository/src/views/MainView.java
@@ -1,7 +1,10 @@
 package com.scrollarchivist.simplescrollarchivistrepository.views;
 import com.scrollarchivist.simplescrollarchivistrepository.models.MainModel;
+import java.awt.Color;
 import java.awt.Dimension;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SpringLayout;
 import javax.swing.SwingUtilities;
 import org.springframework.stereotype.Component;
 /**
@@ -19,14 +22,24 @@ public class MainView {
     // <editor-fold defaultstate="collapsed" desc="Methodes">
     public void show() {
         SwingUtilities.invokeLater(() -> {
-            // Création de la fenêtre (JFrame)
-            JFrame frame = new JFrame(MainModel.APP_TITLE);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            Dimension appSize = new Dimension(MainModel.APP_MIN_WIDTH, MainModel.APP_MIN_HEIGHT);
-            frame.setSize(appSize);
-            frame.setMinimumSize(appSize);
-            frame.setVisible(true);
+            JPanel body = newBody();
         });
+    }
+    private JFrame newWindow() {
+        JFrame window = new JFrame(MainModel.APP_TITLE);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Dimension appSize = new Dimension(MainModel.APP_MIN_HEIGHT, MainModel.APP_MIN_WIDTH); //Mode portrait
+        window.setSize(appSize);
+        window.setMinimumSize(appSize);
+        window.setVisible(true);
+        return window;
+    }
+    private JPanel newBody() {
+        JFrame window = newWindow();
+        JPanel body = new JPanel(new SpringLayout());
+        body.setBackground(Color.BLACK);
+        window.setContentPane(body);
+        return body;
     }
     // </editor-fold>
     // <editor-fold defaultstate="collapsed" desc="Getter">
